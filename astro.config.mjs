f6ead0481474c0efc5a0c44bee3ab71c82bc3494
evanssmaina3 <evanssmaina3@gmail.com>
2023-08-26 15:24:01 +0300

    adding vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://evanssmaina3.github.io',
   base: '/portfolio',
+  output: "server",
+  adapter: vercel()
 });
